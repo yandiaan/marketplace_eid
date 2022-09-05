845a5d08d6ff38f69391e4d0ba57bcccc9cb97e8
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-09-05 13:23:42 +0700

    feat: add login page

--- application/controllers/Auth/Login.php
+++ application/controllers/Auth/Login.php
@@ -4,6 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Login extends CI_Controller
 {
     public function index(){
-        echo 'login';
+        $data = ['content' => 'auth/login'];
+        $this->load->view('layouts/auth', $data);
     }
 }
