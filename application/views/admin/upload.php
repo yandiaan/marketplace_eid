037839ccbf5eab793d11e72bed12160e21c5ae1c
Edokur <edok7179@gmail.com>
2022-09-13 10:33:00 +0700

    file index folder public/upload

--- application/views/admin/upload.php
+++ application/views/admin/upload.php
@@ -1,10 +1,10 @@
+<h2 class="mb-5">Upload Banner</h2>
 <?php if (isset($response)) echo $response; ?></b>
 <form method='POST' action='<?php echo base_url('api/admin/upload_file/save'); ?>' enctype='multipart/form-data'>
 
     <input type='file' name='inputFile'> <br /><br />
     <button type='submit'>Simpan</button>
 </form>
-
 <div class="table-responsive mt-5">
     <table class="table table-striped table-hover table-sm table-bordered" id="tabel-produk">
         <thead class="bg-primary text-white text-center">
