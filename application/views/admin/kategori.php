7a6ba59ed5bb6d3de79ddf21497f3ac82f0e0189
Edokur <edok7179@gmail.com>
2022-09-15 10:01:25 +0700

    CRUD kategori

--- application/views/admin/kategori.php
+++ application/views/admin/kategori.php
@@ -62,7 +62,7 @@
 </div>
 
 <!-- Modal update-->
-<div class="modal fade" id="modal-update" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal-update-kategori" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
@@ -71,57 +71,16 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="<?php echo site_url('api/admin/product/update') ?>" method="POST">
+            <form action="<?php echo site_url('api/admin/kategori/update') ?>" method="POST">
                 <div class="modal-body">
-                    <input type="hidden" name="id_produkU" id="id_produk" value="">
+                    <input type="hidden" name="id_produk_kategoriU" id="id_produk_kategoriU" value="">
                     <div class="form-group row">
                         <label for="inputEmail3" class="col-sm-2 col-form-label">Nama Produk</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="nama_produkU" id="nama_produk" value="">
+                            <input type="text" class="form-control" name="nama_produk_kategoriU" id="nama_produk_kategoriU" value="">
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Deskripsi</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="deskripsiU" id="deskripsi" value="">
-                        </div>
-                    </div>
-                    <!-- <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Spesifikasi</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword3" name="spesifikasi">
-                        </div>
-                    </div> -->
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Brand</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="brandU" id="brand">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Harga</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="hargaU" id="harga">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Berat</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="beratU" id="berat">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Lebar</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="lebarU" id="lebar">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-2 col-form-label">Tinggi</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="tinggiU" id="tinggi">
-                        </div>
-                    </div>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
@@ -168,9 +127,9 @@
             dataType: 'json',
             success: function(response) {
                 console.log(response);
-                $('#id_produk').val(response.id_produk);
-                $('#nama_produk').val(response.nama_produk);
-                $('#modal-update').modal('show');
+                $('#id_produk_kategoriU').val(response.id_produk_kategori);
+                $('#nama_produk_kategoriU').val(response.nama_kategori);
+                $('#modal-update-kategori').modal('show');
             },
             error: function(xmlresponse) {
                 console.log(xmlresponse);
