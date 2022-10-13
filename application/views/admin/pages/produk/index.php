03931b0fffbb4dc16a9b328b709bf3181526bd1a
Edokur <edok7179@gmail.com>
2022-10-13 15:00:32 +0700

    menghilngkan modal tidak berguna

--- application/views/admin/pages/produk/index.php
+++ application/views/admin/pages/produk/index.php
@@ -12,7 +12,6 @@
         <div class="card">
             <div class="card-body">
                 <div class="table-responsive">
-                    <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Aw, yeah!</button>
                     <table class="table dataTable" id="datatable">
                         <thead>
                             <tr>
@@ -32,23 +31,3 @@
         </div>
     </div>
 </section>
-
-<div class="modal fade" tabindex="-1" role="dialog" id="exampleModal">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Modal body text goes here.</p>
-            </div>
-            <div class="modal-footer bg-whitesmoke br">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-    </div>
-</div>
